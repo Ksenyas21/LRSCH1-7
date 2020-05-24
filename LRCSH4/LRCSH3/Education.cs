431f2a6bf2daa827ac2639c2d0828d51b0728c2e
Ksenyas21 <56151288+Ksenyas21@users.noreply.github.com>
2020-05-24 17:59:23 +0300

    Add files via upload

---
+++ LRCSH4/LRCSH3/Education.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRCSH3
+{
+    enum Education
+    {
+        Master, Bachelor, SecondEducation
+    }
+}
---
+++ LRCSH4/LRCSH3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace LRCSH3
+{
+   internal  class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentCollections studentCollection = new StudentCollections();
+            studentCollection.AddStudents(
+                    TestCollections.GetStudent(5),
+                    TestCollections.GetStudent(2),
+                    TestCollections.GetStudent(1),
+                    TestCollections.GetStudent(3),
+                    TestCollections.GetStudent(4)
+                );
+
+           Console.WriteLine("studentCollection default: \n {0}\n", string.Join(" ; ", studentCollection.Students.Select(x => x._Name).ToArray()));
+
+            studentCollection.SortByName();
+            Console.WriteLine("Sorted by Name: \n {0}\n", string.Join(" ; ", studentCollection.Students.Select(x => x._Name).ToArray()));
+            studentCollection.SortByDate();
+            Console.WriteLine("Sorted by Date: \n {0}\n", string.Join(" ; ", studentCollection.Students.Select(x => x._Name).ToArray()));
+            studentCollection.GetMaxMiddleScore();
+            Console.WriteLine("Sorted by miiddle : \n {0}\n", string.Join(" ; ", studentCollection.Students.Select(x => x._Name).ToArray()));
+            Console.WriteLine("Maximum middle Score: {0}\n", studentCollection.GetMaxMiddleScore());
+             Console.WriteLine("Person with Education = Master :\n {0}\n",string.Join(" ; ", studentCollection.GetMasterStudents().Select(x => x._Name).ToArray()));
+
+            int value = 3;
+            Console.WriteLine("Students with middle Score more then {0}:\n {1}\n", value,
+                string.Join(" ; ", studentCollection.AverageMarkGroup(value).Select(x => x._Name).ToArray()));
+
+            TestCollections test = new TestCollections(3);
+            Console.WriteLine("Searching time:");
+            test.MeasureTime();
+            Console.ReadKey();
+
+        }
+    }
+}
